c8d9dd47599b9724f036e2a582a0657324c23c90
CeansBzh <ebriantais@orange.fr>
2022-10-11 20:39:50 +0200

    Add Laravel Breeze

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
+    return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/auth.php';
