95320da985dcae09f28b153844c6ed613471ce3d
CeansBzh <ebriantais@orange.fr>
2022-10-13 19:07:48 +0200

    Create album when photo uploaded

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Album;
 use App\Models\Photo;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
@@ -27,15 +28,15 @@ class PhotoController extends Controller
      */
     public function create()
     {
-        return view('photo.create')->with('albums', \App\Models\Album::all()
+        $albums = \App\Models\Album::all()
+            ->sortBy('date')
             ->groupBy([
                 function ($val) {
                     return $val->date->format('Y');
                 },
-                function ($val) {
-                    return $val->date->format('m');
-                },
-            ]));
+            ]);
+
+        return view('photo.create')->with('albums', $albums);
     }
 
     /**
@@ -49,21 +50,36 @@ class PhotoController extends Controller
         $request->validate([
             'files' => 'required|array',
             'files.*' => 'required|image|mimes:png,jpg,jpeg,gif',
-            'album' => 'required_without_all:albumName,albumDesc,albumDate|integer',
-            'albumName' => 'required_without:album|string',
-            'albumDesc' => 'required_without:album|string',
-            'albumDate' => 'required_without:album|date',
+            'album' => 'integer|exists:albums,id|nullable',
+            'albumTitle' => 'string|nullable',
+            'albumDesc' => 'exclude_if:albumTitle,null|required|string',
+            'albumDate' => 'exclude_if:albumTitle,null|required|date',
         ]);
+
+        $albumid = null;
+        if ($request->has('album')) {
+            $albumid = $request->input('album');
+        } elseif ($request->has('albumTitle') && isset($request->albumTitle)) {
+            $album = new Album();
+            $album->title = $request->input('albumTitle');
+            $album->description = $request->input('albumDesc');
+            $album->date = \Carbon\Carbon::createFromFormat('Y-m',$request->input('albumDate'))->startOfMonth();
+            $album->save();
+            $albumid = $album->id;
+        }
 
         foreach ($request->file('files') as $file) {
             $path = $file->store('photos', 'public');
             $photo = Photo::create([
-                'album_id' => 1, // TODO gestion albums
                 'title' => $file->getClientOriginalName(),
                 'path' =>  Storage::url($path),
                 'legend' => $request->legend,
                 'taken' => $request->taken,
             ]);
+            if (isset($albumid)) {
+                $photo->album()->associate($albumid);
+                $photo->save();
+            }
         }
 
         return back()->with('success', 'Photos mises en ligne avec succès !');
--- resources/views/photo/create.blade.php
+++ resources/views/photo/create.blade.php
@@ -20,22 +20,27 @@
         </div>
         @enderror
         <hr class="mt-4 border border-black">
-        <div class="my-4">
+        @if(!$albums->isEmpty())
+        <div class="my-4 relative">
             <label for="album" class="sr-only">Liste des albums de photos</label>
             <select name="album" id="album"
-                class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('photo') border-red-500 @enderror">
-                <option hidden disabled selected value>Lier les photos à un album</option>
-                <optgroup label="Animaux à 4-jambes">
-                    <option value="Chien">Chien</option>
-                    <option value="chat">Chat</option>
-                    <option value="hamster" disabled>Hamster</option>
+                class="bg-gray-100 border-2 w-full p-4 pr-12 rounded-lg truncate @error('album') border-red-500 @enderror">
+                <option hidden disabled selected>Lier les photos à un album ?</option>
+                @foreach ($albums as $year => $albumPerYear)
+                <optgroup label="{{ $year }}">
+                    @foreach ($albumPerYear as $album)
+                    <option value="{{ $album->id }}">{{ $album->title }}</option>
+                    @endforeach
                 </optgroup>
-                <optgroup label="Animaux volants">
-                    <option value="perroquet">Perroquet</option>
-                    <option value="macaw">Macaw</option>
-                    <option value="albatros">Albatros</option>
-                </optgroup>
+                @endforeach
             </select>
+            <button type="button" id="clearBtn" class="absolute top-1/3 right-9 text-slate-500"><svg
+                    xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
+                    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                    class="feather feather-x">
+                    <line x1="18" y1="6" x2="6" y2="18"></line>
+                    <line x1="6" y1="6" x2="18" y2="18"></line>
+                </svg></button>
             @error('album')
             <div class="text-red-500 mt-2 text-sm">
                 {{ $message }}
@@ -43,12 +48,15 @@
             @enderror
         </div>
         <p>Ou créer un nouvel album :</p>
+        @else
+        <p class="mt-4">Vous pouvez créer un nouvel album et y associer les photos :</p>
+        @endif
         <div class="my-4">
-            <label for="albumName" class="sr-only">Titre</label>
-            <input type="text" name="albumName" id="albumName" placeholder="Titre de l'album"
-                class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('albumName') border-red-500 @enderror"
-                value="{{ old('albumName') }}">
-            @error('albumName')
+            <label for="albumTitle" class="sr-only">Titre</label>
+            <input type="text" name="albumTitle" id="albumTitle" placeholder="Titre de l'album"
+                class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('albumTitle') border-red-500 @enderror"
+                value="{{ old('albumTitle') }}">
+            @error('albumTitle')
             <div class="text-red-500 mt-2 text-sm">
                 {{ $message }}
             </div>
@@ -56,7 +64,8 @@
         </div>
         <div class="mb-4">
             <label for="albumDesc" class="sr-only">Description de l'album</label>
-            <textarea name="albumDesc" id="albumDesc" cols="30" rows="5" placeholder="Description de l'album" maxlength="2048"
+            <textarea name="albumDesc" id="albumDesc" cols="30" rows="5" placeholder="Description de l'album"
+                maxlength="2048"
                 class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('albumDesc') border-red-500 @enderror">{{ old('albumDesc') }}</textarea>
             @error('albumDesc')
             <div class="text-red-500 mt-2 text-sm">
@@ -65,10 +74,10 @@
             @enderror
         </div>
         <div class="mb-4">
-            <label for="albumDate" class="sr-only">Mois et année des photos</label>
-            <input type="month" name="albumDate" id="albumDate" placeholder="Date de l'album" min="1950-01" max="{{ date('Y-m') }}"
+            <label for="albumDate">Date des photos</label>
+            <input type="month" name="albumDate" id="albumDate" min="1950-01" max="{{ date('Y-m') }}"
                 class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('albumDate') border-red-500 @enderror"
-                value="{{ old('albumDate') ?? date('Y-m') }}">
+                value="{{ old('albumDate') ?? now()->format('Y-m') }}">
             @error('albumDate')
             <div class="text-red-500 mt-2 text-sm">
                 {{ $message }}
@@ -79,4 +88,16 @@
             <button type="submit" class="bg-blue-500 text-white px-4 py-3 rounded font-medium w-full">Envoyer</button>
         </div>
     </form>
+
+    @if(!$albums->isEmpty())
+    @push('scripts')
+    <script type="text/javascript">
+        let select = document.getElementById('album');
+        let clearBtn = document.getElementById('clearBtn');
+        clearBtn.addEventListener('click', function () {
+            select.selectedIndex = 0;
+        });
+    </script>
+    @endpush
+    @endif
 </x-app-layout>
