e8b68a85769cfeec7dae4c3e6d7bb0d1e51501cc
CeansBzh <ebriantais@orange.fr>
2022-10-16 14:36:31 +0200

    Change assets bundling to vite method

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,2 +1,2 @@
-<img src="{{ asset('img/boat.svg') }}" class="mx-auto h-9 sm:h-11" alt="Voilier, logo du site">
+<img src="{{ Vite::asset('resources/images/boat.svg') }}" class="mx-auto h-9 sm:h-11" alt="Voilier, logo du site">
 <p class="text-lg">Akipaj</p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,9 @@ module.exports = {
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
             },
+            backgroundImage: {
+                'hero': 'url(/resources/images/hero.jpg)',
+            }
         },
     },
 
