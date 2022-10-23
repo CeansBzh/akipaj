03dc6927c65ec017426dd301a94f294311b505f0
CeansBzh <ebriantais@orange.fr>
2022-10-23 18:57:59 +0200

    Improve middleware to handle multiple roles and add to kernel

--- app/Http/Middleware/EnsureHasRole.php
+++ app/Http/Middleware/EnsureHasRole.php
@@ -12,12 +12,14 @@ class EnsureHasRole
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @param  string|null  ...$roles
      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
-    public function handle(Request $request, Closure $next, string $role)
+    public function handle(Request $request, Closure $next, ...$roles)
     {
-        if ($request->user()->roles()->where('name', $role)->exists()) {
-            return $next($request);
+        foreach($roles as $role) {
+            if($request->user()->roles()->where('name', $role)->exists())
+                return $next($request);
         }
 
         abort(403);
