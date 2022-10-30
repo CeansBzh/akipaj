9ec6b75988ade4e514180b966e99e7904758b1d1
CeansBzh <ebriantais@orange.fr>
2022-10-30 18:47:33 +0100

    Add soft deletion to users

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,12 +7,13 @@ use App\Models\Role;
 use App\Models\Photo;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
