65f75261275e69597856df501a303436993ef09e
CeansBzh <ebriantais@orange.fr>
2022-12-02 19:44:20 +0100

    Update photo show page look

--- resources/views/photo/show.blade.php
+++ resources/views/photo/show.blade.php
@@ -1,12 +1,56 @@
 <x-app-layout>
-    <h1>Ma photo : {{ $photo->title }}</h1>
-    @if(Session::has('success'))
-    <div class="text-green-600">
-        {{ Session::get('success') }}
-    </div>
-    @endif
-    <img src="{{ $photo->path }}"
-        alt="{{ isset($photo->legend) ? substr($photo->legend, 0, 125) : 'Photo sans légende' }}">
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">{{ $photo->title }}</h2>
+    </x-slot>
 
-    <x-comments :comments="$comments" :commentable="$photo"/>
+    <div class="p-2">
+        <img class="mx-auto" src="{{ $photo->path }}"
+            alt="{{ isset($photo->legend) ? substr($photo->legend, 0, 125) : 'Photo sans légende' }}">
+        <div class="flex bg-white shadow-lg rounded-lg relative -mt-2 mx-auto max-w-md md:max-w-2xl ">
+            <div class="flex items-start px-4 py-6 w-full">
+                <img class="w-12 h-12 rounded-full object-cover mr-4 shadow"
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Circle-icons-profile.svg/512px-Circle-icons-profile.svg.png"
+                    alt="Photo de profil">
+                <div class="grow">
+                    <div class="flex items-center justify-between">
+                        <h2 class="text-lg font-semibold text-gray-900 -mt-1 whitespace-nowrap truncate overflow-hidden">{{ $photo->user->name }}</h2>
+                        <small class="text-sm text-gray-700">Envoyée
+                            {{ $photo->created_at->diffForHumans() }}</small>
+                    </div>
+                    @if (isset($photo->legend))
+                    <p class="mt-3 text-gray-700 text-sm">
+                        {{ $photo->legend }}
+                    </p>
+                    @endif
+                    <div class="mt-4 flex items-center">
+                        <div class="flex mr-4 text-gray-700 text-sm">
+                            <svg fill="none" viewBox="0 0 24 24" class="w-4 h-4 mr-1" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z" />
+                            </svg>
+                            <span>{{ $photo->comments->count() }}</span>
+                        </div>
+                        <div class="flex text-gray-700 text-sm">
+                            <svg fill="none" viewBox="0 0 24 24" class="w-4 h-4 mr-1" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12" />
+                            </svg>
+                            <span>Partager</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="py-6">
+        <div class="max-w-7xl mx-auto flex-col space-y-4 sm:px-6 lg:px-8">
+
+            <div class="p-4 sm:p-8 bg-white shadow mx-auto max-w-3xl sm:rounded-lg">
+                <livewire:comments :comments="$photo->comments" :commentable="$photo" />
+            </div>
+
+        </div>
+    </div>
+
 </x-app-layout>
