1bceba261537c64fa770b8bc2e1938fac99bf710
CeansBzh <ebriantais@orange.fr>
2022-12-13 21:27:49 +0100

    Add trips and events tabs

--- resources/views/homepage/partials/tabs.blade.php
+++ resources/views/homepage/partials/tabs.blade.php
@@ -1,29 +1,33 @@
 <div x-data="{ openTab: 1 }">
-    <ul class="flex flex-wrap justify-evenly rounded-md bg-white shadow-lg uppercase sm:m-5">
-        <li @click="openTab = 1">
-            <a class="bg-white inline-block py-2 px-4 text-sky-500 hover:text-sky-800 font-semibold" href="#">
+    <ul class="flex flex-wrap text-center rounded-md bg-white shadow-lg sm:m-5">
+        <li class="flex-1" @click="openTab = 1">
+            <button class="bg-white inline-block py-2 px-4 text-sky-500 hover:text-sky-800 font-semibold"
+                :class="{'text-sky-800 border-sky-800 border-b-2': openTab === 1}">
                 Actualités
-            </a>
+            </button>
         </li>
-        <li @click="openTab = 2">
-            <a class="bg-white inline-block py-2 px-4 text-sky-500 hover:text-sky-800 font-semibold" href="#">
-                Photos
-            </a>
-        </li>
-        <li @click="openTab = 3">
-            <a class="bg-white inline-block py-2 px-4 text-sky-500 hover:text-sky-800 font-semibold" href="#">
-                Dernières sorties
-            </a>
-        </li>
-        <li @click="openTab = 3">
-            <a class="bg-white inline-block py-2 px-4 text-sky-500 hover:text-sky-800 font-semibold" href="#">
+        <li class="flex-1" @click="openTab = 2">
+            <button class="bg-white inline-block py-2 px-4 text-sky-500 hover:text-sky-800 font-semibold"
+                :class="{'text-sky-800 border-sky-800 border-b-2': openTab === 2}">
                 Programme
-            </a>
+            </button>
+        </li>
+        <li class="flex-1" @click="openTab = 3">
+            <button class="bg-white inline-block py-2 px-4 text-sky-500 hover:text-sky-800 font-semibold"
+                :class="{'text-sky-800 border-sky-800 border-b-2': openTab === 3}">
+                Dernières sorties
+            </button>
         </li>
     </ul>
     <div class="p-5" x-cloak>
         <div x-show="openTab === 1">
             @include('homepage.partials.news')
+        </div>
+        <div x-show="openTab === 2">
+            @include('homepage.partials.events')
+        </div>
+        <div x-show="openTab === 3">
+            @include('homepage.partials.trips')
         </div>
     </div>
 </div>
