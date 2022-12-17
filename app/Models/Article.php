8640c0fc21e84b2c26bc4d4b16ccbbce70116c9b
CeansBzh <ebriantais@orange.fr>
2022-12-17 21:12:59 +0100

    Create article model

---
+++ app/Models/Article.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+        'slug',
+        'body_md',
+        'summary_md',
+        'body_html',
+        'summary_html',
+        'online',
+    ];
+}
