9fc6d707f18872afb18d1e936a0214e770113d27
CeansBzh <ebriantais@orange.fr>
2022-12-30 11:04:13 +0100

    Reduce size of section

--- resources/views/homepage/partials/useful-links.blade.php
+++ resources/views/homepage/partials/useful-links.blade.php
@@ -5,34 +5,34 @@
         </h3>
     </div>
     <div class="flex flex-wrap justify-around mt-10">
-        <a href="https://www.windy.com/" target="_blank" rel="noreferrer" class="mx-5 mb-14 sm:mx-14">
+        <a href="https://www.windy.com/" target="_blank" rel="noreferrer" class="mx-2 mb-8 sm:mx-4">
             <img src="{{ Vite::asset('resources/views/homepage/images/links/windy.webp') }}" alt="Logo du site Windy"
-                class="h-16">
+                class="h-8 lg:h-10">
         </a>
-        <a href="https://directcordage.com/" target="_blank" rel="noreferrer" class="mx-5 mb-14 sm:mx-14">
+        <a href="https://directcordage.com/" target="_blank" rel="noreferrer" class="mx-2 mb-8 sm:mx-4">
             <img src="{{ Vite::asset('resources/views/homepage/images/links/direct-cordage.webp') }}"
-                alt="Logo du site Direct Cordage" class="h-16">
+                alt="Logo du site Direct Cordage" class="h-8 lg:h-10">
         </a>
-        <a href="https://maree.info/" target="_blank" rel="noreferrer" class="mx-5 mb-14 sm:mx-14">
+        <a href="https://maree.info/" target="_blank" rel="noreferrer" class="mx-2 mb-8 sm:mx-4">
             <img src="{{ Vite::asset('resources/views/homepage/images/links/maree-info.webp') }}"
-                alt="Logo du site Marée.info" class="h-16">
+                alt="Logo du site Marée.info" class="h-8 lg:h-10">
         </a>
-        <a href="https://www.shom.fr/" target="_blank" rel="noreferrer" class="mx-5 mb-14 sm:mx-14">
+        <a href="https://www.shom.fr/" target="_blank" rel="noreferrer" class="mx-2 mb-8 sm:mx-4">
             <img src="{{ Vite::asset('resources/views/homepage/images/links/shom.webp') }}" alt="Logo du site Shom"
-                class="h-16">
+                class="h-8 lg:h-10">
         </a>
-        <a href="https://windfinder.com/" target="_blank" rel="noreferrer" class="mx-5 mb-14 sm:mx-14">
+        <a href="https://windfinder.com/" target="_blank" rel="noreferrer" class="mx-2 mb-8 sm:mx-4">
             <img src="{{ Vite::asset('resources/views/homepage/images/links/windfinder.webp') }}"
-                alt="Logo du site Windfinder" class="h-16">
+                alt="Logo du site Windfinder" class="h-8 lg:h-10">
         </a>
-        <a href="https://www.windguru.cz/" target="_blank" rel="noreferrer" class="mx-5 mb-14 sm:mx-14">
+        <a href="https://www.windguru.cz/" target="_blank" rel="noreferrer" class="mx-2 mb-8 sm:mx-4">
             <img src="{{ Vite::asset('resources/views/homepage/images/links/windguru.webp') }}"
-                alt="Logo du site Windguru" class="h-16">
+                alt="Logo du site Windguru" class="h-8 lg:h-10">
         </a>
         <a href="https://voilesetvoiliers.ouest-france.fr/" target="_blank" rel="noreferrer"
-            class="mx-5 mb-14 sm:mx-14">
+            class="mx-2 mb-8 sm:mx-4">
             <img src="{{ Vite::asset('resources/views/homepage/images/links/voiles-et-voiliers.webp') }}"
-                alt="Logo du site Voiles et voiliers" class="h-16">
+                alt="Logo du site Voiles et voiliers" class="h-8 lg:h-10">
         </a>
     </div>
 </section>
