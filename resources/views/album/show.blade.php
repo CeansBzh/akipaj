3d40dcc2cd422d2cf8c9db4e2e5206095adf0f9e
CeansBzh <ebriantais@orange.fr>
2023-01-07 11:11:08 +0100

    Replace button by dropdown menu

--- resources/views/album/show.blade.php
+++ resources/views/album/show.blade.php
@@ -5,22 +5,44 @@
                 <span class="font-semibold ">{{ $album->title }}</span> - <span class="text-lg">{{
                     $album->date->translatedFormat('F Y') }}</span>
             </h2>
-            <div class="flex flex-col space-y-2 w-full xs:w-auto sm:flex-row sm:space-x-2 sm:space-y-0">
-                <x-primary-link href="{{ route('photos.create', $album) }}" class="mx-auto">
+            <div class="flex w-full justify-between xs:w-auto sm:space-x-3">
+                <x-primary-link href="{{ route('photos.create', $album) }}">
                     Ajouter mes photos
                 </x-primary-link>
-                <x-secondary-link href="{{ route('albums.edit', $album) }}" class="mx-auto">
-                    Modifier l'album
-                </x-secondary-link>
-                @if($album->photos->count() === 0)
-                <form method="post" action="{{ route('albums.destroy', $album) }}" class="mx-auto">
-                    @csrf
-                    @method('delete')
-                    <x-secondary-button type="submit" class="border-red-600 text-red-600 w-full">
-                        Supprimer l'album
-                    </x-secondary-button>
-                </form>
-                @endif
+                <div class="flex items-center">
+                    <x-dropdown align="right" width="48">
+                        <x-slot name="trigger">
+                            <button class="group">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none"
+                                    stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                    stroke-linejoin="round" class="h-6 text-gray-900">
+                                    <circle cx="12" cy="12" r="1"></circle>
+                                    <circle cx="12" cy="5" r="1"></circle>
+                                    <circle cx="12" cy="19" r="1"></circle>
+                                </svg>
+                            </button>
+                        </x-slot>
+
+                        <x-slot name="content">
+                            @can('update', $album)
+                            <x-dropdown-link :href="route('albums.edit', $album)">
+                                Modifier l'album
+                            </x-dropdown-link>
+                            @endcan
+                            @can('delete', $album)
+                            <form method="post" action="{{ route('albums.destroy', $album) }}" class="w-full">
+                                @csrf
+                                @method('delete')
+                                <button class="w-full text-left">
+                                    <x-dropdown-link class="text-red-600">
+                                        Supprimer l'album
+                                    </x-dropdown-link>
+                                </button>
+                            </form>
+                            @endcan
+                        </x-slot>
+                    </x-dropdown>
+                </div>
             </div>
         </div>
         <hr class="mt-2 mb-4">
