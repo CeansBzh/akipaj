b9aaf52a3199cc670d1401780acd51c124c3455a
CeansBzh <ebriantais@orange.fr>
2023-01-14 15:36:37 +0100

    Add width limitation to album gallery

--- resources/views/album/show.blade.php
+++ resources/views/album/show.blade.php
@@ -2,8 +2,8 @@
     <x-slot name="header">
         <div class="flex flex-wrap justify-between items-center">
             <h2 class="text-xl text-gray-800 leading-tight mb-2 md:mb-0">
-                <span class="font-semibold ">{{ $album->title }}</span> - <span class="text-lg">{{
-                    $album->date->translatedFormat('F Y') }}</span>
+                <span class="font-semibold ">{{ $album->title }}</span> - <span
+                    class="text-lg">{{ $album->date->translatedFormat('F Y') }}</span>
             </h2>
             <div class="flex w-full justify-between xs:w-auto sm:space-x-3">
                 <x-primary-link href="{{ route('photos.create', $album) }}">
@@ -25,20 +25,20 @@
 
                         <x-slot name="content">
                             @can('update', $album)
-                            <x-dropdown-link :href="route('albums.edit', $album)">
-                                Modifier l'album
-                            </x-dropdown-link>
+                                <x-dropdown-link :href="route('albums.edit', $album)">
+                                    Modifier l'album
+                                </x-dropdown-link>
                             @endcan
                             @can('delete', $album)
-                            <form method="post" action="{{ route('albums.destroy', $album) }}" class="w-full">
-                                @csrf
-                                @method('delete')
-                                <button class="w-full text-left">
-                                    <x-dropdown-link class="text-red-600">
-                                        Supprimer l'album
-                                    </x-dropdown-link>
-                                </button>
-                            </form>
+                                <form method="post" action="{{ route('albums.destroy', $album) }}" class="w-full">
+                                    @csrf
+                                    @method('delete')
+                                    <button class="w-full text-left">
+                                        <x-dropdown-link class="text-red-600">
+                                            Supprimer l'album
+                                        </x-dropdown-link>
+                                    </button>
+                                </form>
                             @endcan
                         </x-slot>
                     </x-dropdown>
@@ -58,16 +58,18 @@
     </x-slot>
 
     <section class="text-gray-700">
-        @if(count($photoIds) > 0)
-        <livewire:lightbox />
-        <livewire:gallery :photoIds="$photoIds" />
+        @if (count($photoIds) > 0)
+            <livewire:lightbox />
+            <div class="max-w-screen-2xl mx-auto">
+                <livewire:gallery :photoIds="$photoIds" />
+            </div>
         @else
-        <div class="flex flex-col items-center justify-center h-64">
-            <p class="text-xl mb-5">Aucune photo de publiée.</p>
-            <x-primary-link href="{{ route('photos.create', $album) }}">
-                Ajouter mes photos
-            </x-primary-link>
-        </div>
+            <div class="flex flex-col items-center justify-center h-64">
+                <p class="text-xl mb-5">Aucune photo de publiée.</p>
+                <x-primary-link href="{{ route('photos.create', $album) }}">
+                    Ajouter mes photos
+                </x-primary-link>
+            </div>
         @endif
     </section>
 </x-member-layout>
